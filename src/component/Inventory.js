2e16a4db99077b07767ae30ab4a60746a4acbdd1
HaMa05 <vanha.jungle@gmail.com>
2022-03-07 02:06:37 +0700

    route and finish v09

---
+++ src/component/Inventory.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Inventory = (props) => <div className='inventory'>Inventory</div>;
+
+export default Inventory;
--- src/component/StorePicker.js
+++ src/component/StorePicker.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-class StorePicker extends React.Component {
-	render() {
-		return (
-			<form className='store-selector'>
-				<h2>Please Enter A Store</h2>
-				<input type='text' required placeholder='Store name' />
-				<button type='submit'>Visit Store</button>
-			</form>
-		);
-	}
-}
+const StorePicker = (props) => (
+	<form className='store-selector'>
+		<h2>Please Enter A Store</h2>
+		<input type='text' required placeholder='Store name' />
+		<button type='submit'>Visit Store</button>
+	</form>
+);
 
 export default StorePicker;
